8cc7ecd8b26c45fec870b9bacce2c14ad9720bc3
waledalsaber123-ui <waled.alsaber123@gmail.com>
2026-03-06 18:38:04 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+export const CONFIG = {
+
+PORT: process.env.PORT || 3000,
+
+OPENAI_KEY: process.env.OPENAI_KEY,
+
+GREEN_TOKEN: process.env.GREEN_TOKEN,
+
+ID_INSTANCE: process.env.ID_INSTANCE,
+
+GROUP_ID: "120363407952234395@g.us",
+
+DELIVERY_SHEET_URL: process.env.DELIVERY_SHEET_URL,
+
+API_URL: `https://7103.api.greenapi.com/waInstance${process.env.ID_INSTANCE}`
+
+}
